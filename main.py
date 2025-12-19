0a46e14f6492afa990557a84cc363b1ef3fed729
walt <89993634+WaltDub@users.noreply.github.com>
2025-12-19 23:06:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from textwrap import shorten
 API_SECRET = os.environ["API_SECRET"]
 BASE_URL = os.environ["APPSCRIPT_URL"]
 
-WHISPER_BIN = Path("./whisper.cpp/main")
+WHISPER_BIN = Path("./whisper.cpp/build/bin/whisper")
 WHISPER_MODEL = Path("./models/ggml-base.en.bin")
 
 LLAMA_BIN = Path("./llama.cpp/build/bin/llama")
